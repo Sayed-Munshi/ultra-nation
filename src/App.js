c0e7003b88c1b7d0acd70207cd3420dbae7dcbab
Sayed-Munshi <mdsayed3000@gmail.com>
2021-06-01 10:57:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
+import animalData from './data/data.json';
 
 function App() {
+ const [countries, setCountries] = useState([]);
+ const [cart, setCart] = useState([]);
+ const [animal, setAnimal] = useState([]);
+ 
+
+ useEffect(() =>{
+  setAnimal(animalData);
+  console.log(animalData);
+ },[])
+
+ useEffect(()=>{
+   fetch('https://restcountries.eu/rest/v2/all')
+   .then(res => res.json())
+   .then(data => setCountries(data)
+  
+    // const names = data.map(country => country.name)
+   ) 
+ }, [])
+
+ const handleAddCountry = (country) => {
+   const newCart = [...cart, country];
+   setCart (newCart);
+  }
+
   return (
     <div className="App">
+       <h1>countries loaded:{countries.length}</h1>
+       <h4>Country added: {cart.length}</h4>
+       <Cart cart = {cart}></Cart>
+       
+          {
+            countries.map(country => <Country country = {country} handleAddCountry={handleAddCountry} key = {country.alpha3Code}> </Country>)
+          }
+       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
